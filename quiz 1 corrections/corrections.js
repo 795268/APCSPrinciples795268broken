334b96ba921a9e797d95a4eaa490ee589519dc23
795268 <42563195+795268@users.noreply.github.com>
2019-09-10 08:48:07 -0700

    corrections

---
+++ quiz 1 corrections/corrections.js
@@ -0,0 +1,24 @@
+//quiz 1 corrections
+//elena campell
+
+
+//#1
+var num = 1;
+if(num < 4){
+  console.log("Peace");
+}
+console.log("War");
+
+//prints
+//Peace
+//War
+
+//#2
+for(var i= 14; i > 8; i--){
+  i= i-4;
+  console.log(i);
+}
+
+//prints
+//10
+//5
