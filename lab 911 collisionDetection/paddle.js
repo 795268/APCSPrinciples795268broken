d4e9f244a3b8e29860b5ea50e610eaef0eee1472
795268 <42563195+795268@users.noreply.github.com>
2019-09-11 08:50:51 -0700

    started paddle

---
+++ lab 911 collisionDetection/paddle.js
@@ -0,0 +1,13 @@
+Class Paddle {
+  constructor(x, y, w, h){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h= h;
+    this.clr = color(random(255), random(255), random(255));
+  }
+
+
+
+
+} //*** end class
