38fd8057f367a79a74777d5eaafd88bc5ec42693
795268 <42563195+795268@users.noreply.github.com>
2019-10-06 12:11:30 -0700

    sort

--- Labs/lab104 mysort/sketch.js
+++ Labs/lab104 mysort/sketch.js
@@ -4,7 +4,7 @@
 //  The setup function function is called once when your program begins
 
 var list = [ 5, 9, 10, 6, 2, 1, 3, 0];
-var order[];
+var order =[0,0,0,0,0,0,0,0];
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
@@ -24,17 +24,18 @@ function mySort(){
 var small = 0;
 var i = 0;
 while(i< list.length){ //keeps track of length of sorted array
-          for (var x = 0; x<list.length; x++){ //traverses nums
+          for (var x = 0; x<list.length; x++){ //traverses list
 
               if (list[x] === small){ // if the value = small
-                  order[i] =small; //assign that value to order
+                  order[x].push(small); //assign that value to order
                   i++; //increment to keep track of length of sorted array
               }
-        //      compare++;
+
           }
           small ++; // increment small - looking for next biggest int
+
       }
-      for (var j = 0; j< order.length; j++){
-        console.log(order[i]); 
-      }
+      // for (var j = 0; j< order.length; j++){
+      //   console.log(order[i]);
+      // }
     }
