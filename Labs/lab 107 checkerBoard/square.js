6d06c2856879fe8f2183dd481922e45fd9b1e56b
795268 <42563195+795268@users.noreply.github.com>
2019-10-07 10:27:35 -0700

    checker

--- Labs/lab 107 checkerBoard/square.js
+++ Labs/lab 107 checkerBoard/square.js
@@ -7,14 +7,17 @@
 */
 class Square{
 
-constructor (x, y){
+constructor (x, y, clr){
   this.x = x;
   this.y = y ;
+  this.clr = clr;
+
 }
 
  render(){
+   fill (this.clr);
    rect (this.x, this.y, 100, 100);
-  
+
 }
 
 
