7506bdf0d4651bddaa55023dbd13fb0e6a048d83
795268 <42563195+795268@users.noreply.github.com>
2019-10-08 21:28:22 -0700

    finished checker

--- Labs/lab 107 checkerBoard/sketch.js
+++ Labs/lab 107 checkerBoard/sketch.js
@@ -1,52 +1,46 @@
 /*
-**Lab 911:CheckerBoard
+**Elena Campell
+Lab 911:CheckerBoard
 **
 **
 */
 
 
-//  Global variables
-
-//  Global variables
-var squares = [] ; // declare b1 as a global variable
-
-
+var squares = [];
 // put setup code here
-function setup() {
+function setup(){
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(235);
+  // Create a function that loads the squares into an
+  // array of square objects and call the function
+
   loadSquares();
-  //  traverse array and run each ball
-
+    for(var i=0;i<squares.length;i++){
+      squares[i].render();
+    }
 }
-function draw(){
-  runSquares();
-}
-
-
-function loadSquares(){
-  for(var i = 0; i < squares.length; i++){
-    squares [i]= [];
-
-    for (var j = 0; j< squares.length; j ++){
-      if (i%2 === 0 && j%2 === 0 ){
-      squares[i][j]=   new Square (100*(i+1), 100*i, color (250, 0 , 0));
-       } else if (i%2 === 0 && j%2 === 1){
-        squares[i][j]=  new Square (100*(i+1), 100*i, color (0, 0 , 250));
-       } else if ( i %2 === 1 && j %2 === 0 ){
-        squares[i][j]=  new Square (100*(i+1), 100*i, color (250, 0 , 0));
-       }else if (i%2 === 1 && j%2 === 1){
-        squares[i][j]=  new Square (100*(i+1), 100*i, color (0, 0 , 250));
-       }
+  //  traverse array
+  function loadSquares(){
+    for (var i=0; i<8; i++){
+      for(var j=0; j<8; j++){ //nested for loop
+        if(i%2===0){ //even number?
+          if(j%2===0){
+            var clr=color(255, 0, 0); //if even, blue color
+          }
+          if(j%2!==0){ //odd number?
+            var clr=color(0); //if odd, purple color
+          }
+        }
+        if(i%2!==0){
+          if(j%2!==0){
+            var clr=color(255,0, 0);
+          }
+          if(j%2===0){
+            var clr=color(0);
+          }
+        }
+        squares[j+(8*i)]= new Square(100*j,100*i,100,100, clr); //loads array with square objects
+      }
     }
   }
-}
-
-function runSquares(){
-  for(var i = 0; i < squares.length; i++){
-    for (var j = 0; j< squares.length; j ++){
-      squares [i][j].render();
-    }
-  }
-}
--- Labs/lab 107 checkerBoard/square.js
+++ Labs/lab 107 checkerBoard/square.js
@@ -2,23 +2,26 @@
 
 /*
 **  Square Constructor Function::
-**  eettlin
+**  elena campell
 **  Sept. 11, 2018
 */
 class Square{
 
-constructor (x, y, clr){
-  this.x = x;
-  this.y = y ;
-  this.clr = clr;
+ constructor(x,y,w,h,c){
+   this.loc=createVector(x,y);
+   this.w=w;
+   this.h=h;
+   this.clr= c;
+ } //  End of constructor
+
+run(){
+ this.render();
+ this.update();
 
 }
-
- render(){
-   fill (this.clr);
-   rect (this.x, this.y, 100, 100);
-
+render(){
+ fill(this.clr);
+ rect(this.loc.x,this.loc.y,this.w,this.h);
 }
 
-
-}
+}// end of Square class
