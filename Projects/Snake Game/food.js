98afbf6d6403564eb2ad6ce4692862122d6ee46e
795268 <42563195+795268@users.noreply.github.com>
2019-11-07 10:29:24 -0800

    trying to eat food

--- Projects/Snake Game/food.js
+++ Projects/Snake Game/food.js
@@ -1,17 +1,26 @@
 class Food{
 
 constructor (x, y, clr){
-  this.loc = createVector(x,y);
+  this.food = createVector(x,y);
   this.clr = clr;
 }
 
 run(){
   this.render();
+  this.update();
 }
 
 render(){
     fill(this.clr);
-    rect(this.loc.x, this.loc.y, 30, 30);
+    rect(this.food.x, this.food.y, 30, 30);
+}
+
+update(){
+  if(snake.head.x === this.food.x &&
+   snake.head.y === this.food.y){
+  this.food.x = Math.floor(random(0,79))*30;
+  this.food.y = Math.floor(random(0,79))*30;
+}
 
 }
 
